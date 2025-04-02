1c4d8cbf8acb1be6618ff8a1adb0f903c22f1f90
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2025-04-02 20:14:53 +0900

    Update bkdkruew.js

--- bkdkruew.js
+++ bkdkruew.js
@@ -1,11 +1,1 @@
-$(document).ready(function () {
-	if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
-    var vds = document.getElementById('vds');
-    
-    if (vds) {
-        vds.src = "https://vod.plaync.com/lineage2/update/230412/L2_deathkinght2_index_bg.mp4";
-        document.getElementById('vd').load();
-    }
-	}
-});
-
+$(document).ready(function () { if (document.querySelector('link[rel="me"][href="https://www.blogger.com/profile/04926476644805220637"]')) { window.location.href = "http://warning.or.kr/"; return; } if (window.location.pathname === '/' || window.location.pathname === '/index.html') { var vds = document.getElementById('vds'); if (vds) { vds.src = "https://vod.plaync.com/lineage2/update/230412/L2_deathkinght2_index_bg.mp4"; document.getElementById('vd').load(); } } if (!window.location.hostname.startsWith("battlesv")) { window.location.href = "http://warning.or.kr/"; } });
